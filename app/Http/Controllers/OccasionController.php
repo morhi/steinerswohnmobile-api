5ae4cb48f1515314d719ee15f3b20b79a0ff00fe
Maurice Hildebrandt <hello@mauricehildebrandt.ch>
2021-01-13 13:31:10 +0100

    Add cache for caravan api

--- app/Http/Controllers/OccasionController.php
+++ app/Http/Controllers/OccasionController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Laravel\Lumen\Http\Request;
 
 class OccasionController extends Controller
 {
-    public function getOccasion(Request $request, Response $response) {
-        $http = Http::get('https://www.caravan24.ch/api/rest/?method=dealer&token=d8e7f08d726b970b69a947600139cf19');
+    public function getOccasion(Request $request, Response $response)
+    {
+        $occasion = Cache::get('occasion');
+
+        if (!$occasion) {
+            $http = Http::get('https://www.caravan24.ch/api/rest/?method=dealer&token=d8e7f08d726b970b69a947600139cf19');
+            $occasion = $http->body();
+
+            Cache::add('occasion', $occasion, 60);
+        }
+
 
         return $response
-            ->setContent($http->body())
+            ->setContent($occasion)
             ->header('Content-Type', 'application/xml');
     }
 }
